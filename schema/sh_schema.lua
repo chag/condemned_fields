308a8f8b2484d360775e7dc42308025d9c775dd6
Daniel <danielga100@hotmail.com>
2014-07-10 16:14:09 +0100

    Initial commit.

---
+++ schema/sh_schema.lua
@@ -0,0 +1,3 @@
+SCHEMA.Name = "Condemned Fields"
+SCHEMA.Author = "MetaMan"
+SCHEMA.Description = "A gamemode based on S.T.A.L.K.E.R., Rust, DayZ and many others, in an attempt to bring fun elements from these."
